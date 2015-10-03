32518b7b4dc543ccdbe8ee5a78cbaf09e198e242
Michael Kutý <6du1ro.n@gmail.com>
2015-10-04 01:04:22 +0200

    monkey path admin_sso settings

--- leonardo_admin_sso/__init__.py
+++ leonardo_admin_sso/__init__.py
@@ -29,3 +29,9 @@ class Config(AppConfig):
 
         import leonardo.site
         leonardo.site.leonardo_admin.login_template = 'admin_sso/login.html'
+
+        # monkey path settings
+        from django.conf import settings as django_settings
+        from admin_sso import settings
+        for key in list(LEONARDO_CONFIG.keys()):
+            setattr(settings, key, getattr(django_settings, key, None))
